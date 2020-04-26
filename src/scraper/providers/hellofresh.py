d755842ef7144dd0ee83ca6868816396428a6196
anubhavcodes <contact+code@anubhav.codes>
2020-04-26 14:49:44 +0200

    🥩Fix bug with nutritional info formatting.

--- src/scraper/providers/hellofresh.py
+++ src/scraper/providers/hellofresh.py
@@ -5,55 +5,56 @@ from scraper.providers.utils import format_measurements
 
 
 class HelloFreshScraper(BaseScraper):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.soup = self.get_soup()
 
     def scrape(self) -> Dict:
         return {
-            'name': self.name,
-            'ingredients': self.ingredients,
-            'nutritional_info': self.nutritional_info,
-            'directions': self.directions,
-            'source_url': self.url,
-            'servings': '2 servings',
-            'categories': ['HelloFresh'],
-            'prep_time': '10 minutes',
-            'cook_time': self.cook_time,
-            'total_time': self.cook_time,
+            "name": self.name,
+            "ingredients": self.ingredients,
+            "nutritional_info": self.nutritional_info,
+            "directions": self.directions,
+            "source_url": self.url,
+            "servings": "2 servings",
+            "categories": ["HelloFresh"],
+            "prep_time": "10 minutes",
+            "cook_time": self.cook_time,
+            "total_time": self.cook_time,
         }
 
     @property
     def name(self) -> str:
-        title = self.soup.find('h1').text
-        name = self.soup.find('h4').text
-        return title + ' ' + name
+        title = self.soup.find("h1").text
+        name = self.soup.find("h4").text
+        return title + " " + name
 
     @property
     def ingredients(self) -> str:
-        measurements = ['ml', 'g', 'Stück']
-        parent_tag = self.soup.find('div', {'data-test-id': 'recipeDetailFragment.ingredients'})
+        measurements = ["ml", "g", "Stück"]
+        parent_tag = self.soup.find("div", {"data-test-id": "recipeDetailFragment.ingredients"})
         div = list(parent_tag)[3]
         in_the_box = [format_measurements(tag.text, measurements) for tag in list(div.children)[0]]
         div = list(parent_tag)[4]
         in_your_pantry = [tag.text for tag in list(div.children)[1]]
-        return '\n'.join(in_the_box + in_your_pantry)
+        return "\n".join(in_the_box + in_your_pantry)
 
     @property
     def nutritional_info(self) -> str:
-        measurements = ['Portion', '(kJ)', 'kcal)', ]
-        parent_tag = self.soup.find('div', {'data-test-id': 'recipeDetailFragment.nutrition-values'})
+        measurements = ["Portion", "(kJ)", "kcal)"]
+        parent_tag = self.soup.find("div", {"data-test-id": "recipeDetailFragment.nutrition-values"})
         div = list(parent_tag)[-1]
-        return '\n'.join([format_measurements(tag.text, measurements) for tag in list(div.children)[0]])
+        return "\n".join([format_measurements(tag.text, measurements) for tag in list(div.children)[0] if tag.text])
 
     @property
     def directions(self) -> str:
-        tag = self.soup.find('a', {'data-test-id': 'recipeDetailFragment.instructions.downloadLink'})
+        tag = self.soup.find("a", {"data-test-id": "recipeDetailFragment.instructions.downloadLink"})
         div = list(tag.parents)[0]
-        all_directions = set([t.text for t in div.next_sibling.next_sibling.find('div').findAll('div') if t.text])
-        return '\n\n'.join(sorted([x for x in all_directions if len(x) > 5 and x[0].isdigit()], key=lambda x: int(x[0])))
+        all_directions = set([t.text for t in div.next_sibling.next_sibling.find("div").findAll("div") if t.text])
+        return "\n\n".join(
+            sorted([x for x in all_directions if len(x) > 5 and x[0].isdigit()], key=lambda x: int(x[0]))
+        )
 
     @property
     def cook_time(self) -> str:
-        return self.soup.find('span', {'data-translation-id': 'recipe-detail.preparation-time'}).next.next.text
+        return self.soup.find("span", {"data-translation-id": "recipe-detail.preparation-time"}).next.next.text
--- src/scraper/providers/utils.py
+++ src/scraper/providers/utils.py
@@ -1,4 +1,5 @@
 def format_measurements(text, measurements):
     for m in measurements:
         if m in text:
-            return text[:text.find(m) + len(m)] + ' ' + text[text.find(m) + len(m):]
+            return text[: text.find(m) + len(m)] + " " + text[text.find(m) + len(m) :]
+    return text
